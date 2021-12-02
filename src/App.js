c992cd6c3cfd54443d576893c54cc29bc266cb0d
Ashenafi <ashenafibordea@gmail.com>
2021-12-02 13:00:06 -0800

    basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className="container">
-        <Router>
+        <Router basename="/excersise-tracker">
           <Navbar />
           <br />
           <Routes>
